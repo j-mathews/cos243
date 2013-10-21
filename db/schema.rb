ce3a040e8ede832704a286bd4fe39a42a0a4b590
John Mathews <j-mathews@live.com>
2013-10-21 04:11:46 +0000

    15 test still failing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131006192512) do
+ActiveRecord::Schema.define(version: 20131016134356) do
 
   create_table "users", force: true do |t|
     t.string   "username"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20131006192512) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",           default: false
   end
 
 end
